663a720f8ef706f70dbf52adaa8150698c890e34
augshailk <augsahilk@gmail.com>
2024-01-02 08:50:28 +0530

    e-commerce

--- coderswear/pages/signup.js
+++ coderswear/pages/signup.js
@@ -1,77 +1,3 @@
-// import React from 'react'
-// import Link from 'next/link'
-// import { useState } from 'react'
-
-// function Signup() {
-//   const [name, setName] = useState()
-//   const [email, setEmail] = useState()
-//   const [password, setPassword] = useState()
-//   const handleChange= (e) =>{
-//     if(e.target.name == 'name'){
-//       setName(e.target.value)
-//     }else if(e.target.name == 'email'){
-//       setName(e.target.value)
-//     }else if(e.target.name == 'password'){
-//       setName(e.target.value)
-//     }
-//   }
-//   const handleSubmit = async(e) =>{
-//     e.preventDefault()//page will not reload whenever we submit the form.
-
-//     const data = { name, email, password }
-//     let res = await fetch('http://localhost:3000/api/signup', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(data),
-//     })
-//     let response = await res.JSON()
-//     console.log(response)
-//     setEmail('')
-//     setName('')
-//     setPassword('')
-//   }
-//   return (
-//     <section class="bg-gray-50 dark:bg-gray-900">
-//       <div class="flex flex-col items-center justify-center px-6 py-8">
-//         <img class="w-24 h-20 mr-20" src="/favicon1.png" alt="logo" />
-//         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-//           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-//             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-//               Create new account
-//             </h1>
-//             <form onSubmit={handleSubmit} class="space-y-4 md:space-y-6" action="#">
-//               <div>
-//                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-//                 <input value={name} onChange={handleChange} type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
-
-//               </div>
-//               <div>
-//                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-//                 <input value={email} onChange={handleChange} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
-
-//               </div>
-//               <div>
-//                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-//                 <input value={password} onChange={handleChange} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
-//                 {/* <label for="password" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-//                 <input onChange={handleChange} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" /> */}
-//               </div>
-//               <button type="submit" class="w-full text-black bg-pink-400 hover:bg-pink-700 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800">Sign up</button>
-//               <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-//                 Don’t have an account yet? <Link href={'/login'} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</Link>
-//               </p>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default Signup
-
 import React, { useState } from "react";
 import Link from "next/link";
 // import Company from "../public/assets/nav.png";
@@ -111,6 +37,7 @@ const Signup = () => {
     setName('')
     setEmail('')
     setPassword('')
+
   };
 
   return (
